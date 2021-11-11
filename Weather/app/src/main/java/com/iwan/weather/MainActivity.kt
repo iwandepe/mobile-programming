68890c6cba4cda13a4f1299ff5e189f8dda7fe4a
Iwan Dwi Prakoso <iwan.dprakoso@gmail.com>
2021-11-11 08:01:49 +0700

    style(weather): change row layout and data to show

--- Weather/app/src/main/java/com/iwan/weather/MainActivity.kt
+++ Weather/app/src/main/java/com/iwan/weather/MainActivity.kt
@@ -2,19 +2,26 @@ package com.iwan.weather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
-    var dataSet: MutableList<WeatherItem> = mutableListOf()
+    var dataSet: MutableList<DailyItem> = mutableListOf()
     lateinit var recyclerViewAdapter: RecyclerViewAdapter
     lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tvLocation = findViewById(R.id.tvLocation) as TextView
+        val tvTodayDate = findViewById(R.id.tvTodayDate) as TextView
+
+        tvLocation.setText("Kediri")
+        tvTodayDate.setText("Hari ini")
 
         recyclerViewAdapter = RecyclerViewAdapter(dataSet, R.layout.item_weather)
         recyclerView = findViewById(R.id.rvWeather)
@@ -33,14 +40,7 @@ class MainActivity : AppCompatActivity() {
                 println("On Response")
                 if (response.body() != null) {
                     var data: WeatherResponse = response.body()!!
-                    if (data!!.hourly!!.isNotEmpty()) {
-                        println(data)
-                        var newData: MutableList<WeatherItem> = mutableListOf()
-                        for (hourlyData in data!!.hourly!!) {
-                            newData.add(hourlyData!!.weather!!.first()!!)
-                        }
-                        dataSet.addAll(newData)
-                    }
+                    dataSet.addAll(data!!.daily!!.filterNotNull())
                     println(dataSet)
                     if (dataSet != null) {
                         println("Dataset isnt null")
--- Weather/app/src/main/java/com/iwan/weather/RecyclerViewAdapter.kt
+++ Weather/app/src/main/java/com/iwan/weather/RecyclerViewAdapter.kt
@@ -1,23 +1,33 @@
 package com.iwan.weather
 
+import android.annotation.SuppressLint
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
+import java.time.Instant
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class RecyclerViewAdapter (
-    private val dataSet: MutableList<WeatherItem>,
+    private val dataSet: MutableList<DailyItem>,
     private val rowLayout: Int
 ):
     RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val weather: TextView
-        val description: TextView
+        val day: TextView
+        val humidity: TextView
+        val temperature: TextView
 
         init {
+            day = view.findViewById(R.id.tvDay)
             weather = view.findViewById(R.id.tvWeather)
-            description = view.findViewById(R.id.tvDescription)
+            humidity = view.findViewById(R.id.tvHumidity)
+            temperature = view.findViewById(R.id.tvTemperature)
         }
     }
 
@@ -28,10 +38,36 @@ class RecyclerViewAdapter (
         return ViewHolder(view)
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
+    @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
 
-        viewHolder.weather.text = dataSet[position].main
-        viewHolder.description.text = dataSet[position].description
+        viewHolder.weather.text = dataSet[position].weather!![0]!!.main
+        viewHolder.day.text = this.getDateTime(dataSet[position].dt!!.toLong())
+        viewHolder.humidity.text = dataSet[position].humidity.toString() + "%"
+        viewHolder.temperature.text = dataSet[position].temp!!.day.toString()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun getDateTime(timestamp: Long): String? {
+        val secondApiFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'")
+
+        val timestampAsDateString = java.time.format.DateTimeFormatter.ISO_INSTANT
+                .format(java.time.Instant.ofEpochSecond(timestamp))
+
+        val nowInEpoch: Long = Instant.now().epochSecond.toLong()
+
+        val date = LocalDate.parse(timestampAsDateString, secondApiFormat)
+
+        if (date.dayOfWeek.toString() == LocalDate.now().dayOfWeek.toString()) {
+            return "Today"
+        }
+        if (nowInEpoch > timestamp) {
+            return "Yesterday"
+        }
+        else {
+            return(date.dayOfWeek.toString().toLowerCase().capitalize())
+        }
     }
 
     override fun getItemCount() = dataSet.size
